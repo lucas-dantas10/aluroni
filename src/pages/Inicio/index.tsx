1c118d6ac8d128d86ef1d598daf06f276b4c06ab
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-06-13 13:50:05 -0300

    criando pasta inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,14 @@
+import cardapio from "pages/Cardapio/Itens/itens.json";
+
+export function Inicio() {
+    return(
+        <section>
+            <h3>
+                Recomendações da cozinha
+            </h3>
+            <div>
+
+            </div>
+        </section>
+    );
+}
