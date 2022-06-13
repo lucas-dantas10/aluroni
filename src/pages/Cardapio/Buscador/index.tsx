0b254ccc394fdaf822b39fafe7cd458e279664d1
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-06-13 13:51:28 -0300

    corrigindo erros

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Buscador.module.scss";
-import { CgSearch } from "react-icons/cg"
+import { CgSearch } from "react-icons/cg";
 
 interface Props {
     busca: string, 
@@ -7,19 +7,19 @@ interface Props {
 }
 
 export function Buscador({busca, setBusca}: Props) {
-    return (
-        <div className={styles.buscador}>
-            <input 
-            type="text"
-            value={busca}
-            onChange={(evento) => setBusca(evento.target.value)}
-            placeholder="Buscar"
-            autoFocus = {true}
-            />
-            <CgSearch
-                size={20}
-                color="#4C4D5E"
-            />
-        </div>
-    );
+  return (
+    <div className={styles.buscador}>
+      <input 
+        type="text"
+        value={busca}
+        onChange={(evento) => setBusca(evento.target.value)}
+        placeholder="Buscar"
+        autoFocus = {true}
+      />
+      <CgSearch
+        size={20}
+        color="#4C4D5E"
+      />
+    </div>
+  );
 }
