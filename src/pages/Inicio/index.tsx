7723b82ed203c924da58400a0c8d66d97fcd302d
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-06-14 16:23:52 -0300

    criando a página de inicio

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,14 +1,27 @@
-import cardapio from "pages/Cardapio/Itens/itens.json";
+import cardapio from "data/cardapio.json";
+import styles from "./Inicio.module.scss";
+import React from "react";
 
 export function Inicio() {
-    return(
-        <section>
-            <h3>
+  let pratosRecomendados = [...cardapio];
+  pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0, 3);
+  return(
+    <section>
+      <h3 className={styles.titulo}>
                 Recomendações da cozinha
-            </h3>
-            <div>
-
+      </h3>
+      <div className={styles.recomendados}>
+        {pratosRecomendados.map(item => (
+          <div key={item.id} className={styles.recomendado}>
+            <div className={styles.recomendado__imagem}>
+              <img src={item.photo} alt={item.title} />
             </div>
-        </section>
-    );
+            <button className={styles.recomendado__botao}>
+                            Ver mais
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
