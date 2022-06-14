f1502984f5d94e889b9f825ef250cdac14238109
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-06-14 16:26:03 -0300

    adicionando o import do react, para não dar erro no eslint

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,12 +1,12 @@
 import styles from "./Item.module.scss";
-import cardapio from "../itens.json";
+import cardapio from "data/cardapio.json";
 import classNames from "classnames";
 
 type Props = typeof cardapio[0];
 
 export function Item(props: Props) {
-  const {category, description, title, photo, serving, price, size, id} = props;
-  return(
+  const { category, description, title, photo, serving, price, size } = props;
+  return (
     <div className={styles.item}>
       <div>
         <img src={photo} alt={title} className={styles.item__imagem} />
@@ -17,21 +17,19 @@ export function Item(props: Props) {
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={classNames({
-            [styles.item__tipo]: true,
-            [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
-          })}>
+          <div
+            className={classNames({
+              [styles.item__tipo]: true,
+              [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
+            })}
+          >
             {category.label}
           </div>
-          <div className={styles.item__porcao}>
-            {size}g
-          </div>
+          <div className={styles.item__porcao}>{size}g</div>
           <div className={styles.item__qtdpessoas}>
-                        Serve {serving} pessoa{serving === 1 ? "" : "s"}
+            Serve {serving} pessoa{serving === 1 ? "" : "s"}
           </div>
-          <div className={styles.item__valor}>
-                        R$ {price.toFixed(2)}
-          </div>
+          <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
         </div>
       </div>
     </div>
--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,12 +1,13 @@
-import cardapio from "./itens.json";
+import { useEffect, useState } from "react";
+import cardapio from "data/cardapio.json";
 import { Item } from "./Item";
 import styles from "./Itens.module.scss";
-import { useEffect, useState } from "react";
+import React from "react";
 
 interface Props {
-    busca: string,
-    filtro: number | null,
-    ordenador: string
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
 }
 
 export function Itens(props: Props) {
@@ -19,39 +20,37 @@ export function Itens(props: Props) {
   }
 
   function testaFiltro(id: number) {
-    if(filtro !== null) return filtro === id;
+    if (filtro !== null) return filtro === id;
     return true;
   }
 
   function ordenar(novaLista: typeof cardapio) {
-    switch(ordenador) {
+    switch (ordenador) {
     case "porcao":
-      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
 
     case "qtdPessoas":
-      return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-            
+      return novaLista.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+
     case "preco":
-      return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-                
-    default: 
+      return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
+
+    default:
       return novaLista;
     }
-  } 
+  }
 
   useEffect(() => {
-    const novaLista = cardapio.filter(item => testaBusca(item.title) 
-            && testaFiltro(item.category.id));
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
     setLista(ordenar(novaLista));
   }, [busca, filtro, ordenador]);
 
-  return(
+  return (
     <div className={styles.itens}>
-      {lista.map(item => (
-        <Item
-          key={item.id}
-          {...item}
-        />
+      {lista.map((item) => (
+        <Item key={item.id} {...item} />
       ))}
     </div>
   );
